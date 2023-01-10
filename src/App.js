6733d5a94d82519a7c81632f5488c8aad377c434
AliCW <aliwhitrow1@gmail.com>
2023-01-10 10:17:05 +0000

    1-list-all-articles-drafted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import Header from './components/Header'
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Login from './components/Login';
+import Articles from './components/Articles';
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          NC-News
-      </header>
+      <Header />
+      <Login />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/articles" element={<Articles />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Articles.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import * as api from '../api'
+
+export default function Articles() {
+    const [articles, selectArticles] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setIsLoading(true)
+        api.fetchArticles().then(({data}) => {
+            selectArticles(data.articles)
+            setIsLoading(false)
+        })
+    }, []
+    )
+
+    if (isLoading) {
+        return <p className="loading">Loading...</p>
+    }
+
+    return (
+        articles.map((article) => {
+            return (
+        <div key={article.article_id} className="article">
+           <h2 className="article-header">{article.title}</h2>
+           <p className="article-details">author: {article.author}</p>
+           <p className="article-details">topic: {article.topic}</p>
+           <p className="article-details">comments: {article.comments_count}</p>
+           <p className="article-details">votes: {article.votes}</p>
+           <p className="article-details">created: {article.created_at.slice(0, 10)}</p>
+           <p>{console.log(articles)}</p>
+        </div>
+
+            )
+        })
+    )
+}
