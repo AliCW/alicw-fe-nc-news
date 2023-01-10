07c4f9e1cd57576b9114d38dab1b660a590dea21
AliCW <aliwhitrow1@gmail.com>
2023-01-10 12:16:26 +0000

    2-single-article-functionality-drafted

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom"
 import * as api from '../api'
 
 export default function Articles() {
@@ -22,13 +23,13 @@ export default function Articles() {
         articles.map((article) => {
             return (
         <div key={article.article_id} className="article">
-           <h2 className="article-header">{article.title}</h2>
+           <Link to={"/article/" + article.article_id} state={{id: article.article_id}} className="article-header">{article.title}</Link>
            <p className="article-details">author: {article.author}</p>
            <p className="article-details">topic: {article.topic}</p>
            <p className="article-details">comments: {article.comments_count}</p>
            <p className="article-details">votes: {article.votes}</p>
            <p className="article-details">created: {article.created_at.slice(0, 10)}</p>
-           <p>{console.log(articles)}</p>
+           
         </div>
 
             )
---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom'
+import * as api from '../api'
+
+export default function SingleArticle () {
+    const [article, selectArticle] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const location = useLocation()
+    const { id } = location.state
+
+    useEffect(() => {
+        setIsLoading(true)
+        api.fetchSingleArticle(id).then(({data}) => {
+            selectArticle(data.article.article[0])
+            setIsLoading(false)
+        })
+    }, [id]
+    )
+
+    if (isLoading) {
+        return <p className="loading">Loading...</p>
+    }
+
+    return (
+        <div className="article">
+            <h3 className="article-header">{article.title}</h3>
+            <p className="article-body">{article.body}</p>
+            <p className="article-details">author: {article.author}</p>
+            <p className="article-details">topic: {article.topic}</p>
+            <p className="article-details">comments: {article.comments_count}</p>
+            <p className="article-details">votes: {article.votes}</p>
+            <p className="article-details">created: {String(article.created_at).slice(0, 10)}</p>
+        </div>
+    )
+}
