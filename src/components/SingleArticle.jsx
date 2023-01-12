113ca6a60c48409428bd988b936d232c9375df26
AliCW <aliwhitrow1@gmail.com>
2023-01-12 10:47:26 +0000

    error message added to UI

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -30,7 +30,10 @@ export default function SingleArticle () {
             api.incVote(article_id).catch(() => {
                 selectArticle((article) => {
                     return article.map((votes) => {
-                        return { ...votes, votes: article[0].votes - 1 }
+                        return (
+                            
+                            { ...votes, votes: article[0].votes - 1, error: "oops, something went wrong casting your vote. Please refresh & try again"}
+                        )
                     })
                 })
             })
@@ -51,7 +54,7 @@ export default function SingleArticle () {
             api.decVote(article_id).catch(() => {
                 selectArticle((article) => {
                     return article.map((votes) => {
-                        return { ...votes, votes: article[0].votes + 1 }  
+                        return { ...votes, votes: article[0].votes + 1, error: "oops, something went wrong casting your vote. Please refresh & try again"}  
                     })
                 })
             })
@@ -82,6 +85,7 @@ export default function SingleArticle () {
             </p>
             <p className="article-details">created: {String(article[0].created_at).slice(0, 10)}</p>
             <p className="article-details">{article[0].comment_count} comments:</p>
+            <p className="article-details">{article[0].error}</p>
             <SingleArticleComments article={article_id}></SingleArticleComments>
         </div>
     )
