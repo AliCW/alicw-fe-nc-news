3a0664e249b7f7ec47f13cbff71ad8f4c2c6a106
AliCW <aliwhitrow1@gmail.com>
2023-02-08 16:20:19 +0000

    date fixed to represent website format

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -27,12 +27,70 @@ export default function AddComment({ article, selectComments }) {
         setError(false)
 
         api.postArticleComment({username: "jessjelly", body: newComment}, id).then((response) => {
-            console.log(response)
+            const date = new Date();
+            const today = date.toString().slice(4, 15);
+            
+            function monthNumeric(today) {
+                
+                let dateArray = today.split(' ');
+                if (dateArray[0] === 'Jan') {
+                    dateArray.shift()
+                    dateArray.unshift('01')
+                }
+                if (dateArray[0] === 'Feb') {
+                    dateArray.shift()
+                    dateArray.unshift('02')
+                }
+                if (dateArray[0] === 'Mar') {
+                    dateArray.shift()
+                    dateArray.unshift('03')
+                }
+                if (dateArray[0] === 'Apr') {
+                    dateArray.shift()
+                    dateArray.unshift('04')
+                }
+                if (dateArray[0] === 'May') {
+                    dateArray.shift()
+                    dateArray.unshift('05')
+                }
+                if (dateArray[0] === 'Jun') {
+                    dateArray.shift()
+                    dateArray.unshift('06')
+                }
+                if (dateArray[0] === 'Jul') {
+                    dateArray.shift()
+                    dateArray.unshift('07')
+                }
+                if (dateArray[0] === 'Aug') {
+                    dateArray.shift()
+                    dateArray.unshift('08')
+                }
+                if(dateArray[0] === 'Sep') {
+                    dateArray.shift()
+                    dateArray.unshift('09')
+                }
+                if(dateArray[0] === 'Oct') {
+                    dateArray.shift()
+                    dateArray.unshift('10')
+                }
+                if(dateArray[0] === 'Nov') {
+                    dateArray.shift()
+                    dateArray.unshift('11')
+                }
+                if(dateArray[0] === 'Dec') {
+                    dateArray.shift()
+                    dateArray.unshift('12')
+                }
+                let shiftArray = [dateArray[0], dateArray[1]] = [dateArray[1], dateArray[0]]
+                shiftArray.push(dateArray[2])
+                return shiftArray.reverse().join('-')
+            }
+            
             const newComment = {
                 author: 'jessjelly',
                 body: response.data.postedComment[0].body,
                 votes: 0,
-                created_at: Date.now(),
+                created_at: monthNumeric(today),
                 comment_id: response.data.postedComment[0].body,
             }
             selectComments((currComments) => {
@@ -47,6 +105,15 @@ export default function AddComment({ article, selectComments }) {
             setIsLoading(false)
         })
         }
+
+        // const date = new Date();
+// const today = date.toString()
+// const comment = new Comment({
+//   username: req.body.username,
+//   body: req.body.body,
+//   event: req.body.event,
+//   date: today,
+// });
     
     const handleComment = () => {
         setCommentSubmit(true);
@@ -77,3 +144,5 @@ export default function AddComment({ article, selectComments }) {
         </div>
     )
 }
+
+
