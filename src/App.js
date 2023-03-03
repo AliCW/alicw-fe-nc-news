5e5ff8f47ce6d27bea1be73cb3e539608e60dbba
AliCW <aliwhitrow1@gmail.com>
2023-03-03 12:43:10 +0000

    login page implemented, user context required

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Signup from './components/Signup';
 import Articles from './components/Articles';
 import Topics from './components/Topics';
+import Login from './components/Login';
 import ArticlesByTopic from './components/ArticlesByTopic';
 import SingleArticleComments from './components/SingleArticleComments';
 import SingleArticle from './components/SingleArticle';
@@ -13,7 +14,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [currentUser, setCurrentUser] = useState({})
+  const [currentUser] = useState('')
 
   return (
     <div className="App">
@@ -21,12 +22,13 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup user={currentUser} setUser={setCurrentUser}/>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login user={currentUser}/>} />
         <Route path="/articles" element={<Articles />} />
         <Route path="/topics" element={<Topics />} />
         <Route path="/topics/:slug" element={<ArticlesByTopic />} />
         <Route path="/article/:article_id" element={<SingleArticle user={currentUser} />} />
-        <Route path="/article/:article_id/comments" element={<SingleArticleComments />} />
+        <Route path="/article/:article_id/comments" element={<SingleArticleComments user={currentUser} />} />
         <Route path="/*" element={<Error />} />
       </Routes>
     </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,53 @@
-export default function Login() {
+import { useState } from 'react';
+import * as api from '../api'
+
+export default function Login(currentUser, setCurrentUser) {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [signinSuccess, setSigninSuccess] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setIsLoading(true)
+        const userData = {
+            "username": username,
+            "password": password,
+        }
+        document.getElementById("login-form").reset()
+        
+        api.userLogin(userData).then((data) => {
+            console.log(data, '<<<< data here ')
+            console.log(currentUser)
+            setIsLoading(false)
+            setSigninSuccess(true)
+            setCurrentUser(username)
+
+        })
+    }
+
+    if (isLoading) return <p className="loading">Loading...</p>
+
     return (
-      <div>
-        <p className="login">Logged in as: jessjelly</p>
-    </div>
-    );
-  };
-
-
+        <div>
+            <form id="login-form" className="login" onSubmit={handleSubmit}>
+                <label>Username</label>
+                <input
+                    className="input"
+                    type="text"
+                    placeholder="Username"
+                    onChange={(event) => {setUsername(event.target.value)}}
+                />
+                <label>Password</label>
+                <input 
+                    className="input"
+                    type="password"
+                    placeholder="Password"
+                    onChange={(event) => {setPassword(event.target.value)}}
+                />
+                {signinSuccess === true && <p>Login Successfull, welcome{currentUser}</p>}
+                <button className="signup-button" type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
