2f2e3d4a4c4de68da22d50bf2a28e6bd956a39b2
AliCW <aliwhitrow1@gmail.com>
2023-06-17 20:32:01 +0100

    username added to nav bar, dynamic when signed in

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom"
 import { BiDotsVerticalRounded } from "react-icons/bi"
-import { useState } from "react"
+import { useState, useContext } from "react"
+import { UserContext } from '../contexts/UserContext';
 import NavUserPanel from "./NavUserPanel"
 
 export default function Nav() {
     const [open, setOpen] = useState(false)
+    const { username } = useContext(UserContext)
 
     const handleOpen = () => {
         setOpen(!open)
     }
-
 
        return (
         <div >         
@@ -25,12 +26,21 @@ export default function Nav() {
             <Link className="navLink" to="/topics">
                 <button className="navButton">Topics</button>
             </Link>
+            
+            {username === '' ? 
 
-            <button onClick={handleOpen} className="navUser">
-            <BiDotsVerticalRounded className="navDropdown" />
-                {console.log(open)}
-                { open === true && <NavUserPanel /> }
-            </button>
+                <button onClick={handleOpen} className="navUser">
+                    Sign In
+                    <BiDotsVerticalRounded className="navUser" />
+                    { open === true && <NavUserPanel /> }
+                </button> 
+                : 
+                <button onClick={handleOpen} className="navUser">
+                    {username}
+                    <BiDotsVerticalRounded className="navUser" />                
+                    { open === true && <NavUserPanel /> }
+                </button> 
+            }
         </nav>
 
         </div>
